1be30fdeec58a966de444625c81dddfb13ff678d
Lisbeth <lisbethperezcm@gmail.com>
2025-03-15 13:04:25 -0400

    Actualización en WelcomeNotification

---
+++ app/Notifications/WelcomeNotification.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+        ->subject('Bienvenido a nuestra Barbería')
+        ->greeting('Hola, ' . $notifiable->person->first_name . ' 👋')
+        ->line('Gracias por registrarte en nuestra barbería. Estamos felices de tenerte con nosotros.')
+        ->line('Ahora puedes agendar tu primera cita con nosotros y disfrutar de nuestros servicios.')
+        ->line('Si tienes alguna duda, contáctanos. ¡Nos vemos pronto!')
+        ->salutation('Saludos, AlexBarbershop');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
