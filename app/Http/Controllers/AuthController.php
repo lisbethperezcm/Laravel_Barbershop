5a72c8151e397ea98a5610fabdb67a652d6bb2d5
Lisbeth <lisbethperezcm@gmail.com>
2025-03-29 11:21:18 -0400

    Mejoras al controlador de AUTH

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,96 +24,100 @@ class AuthController extends Controller
     {
 
         return DB::transaction(function () use ($request) {
-        // La validación ya se maneja automáticamente con RegisterRequest
-     $request->validated();
-        
-        $role = Role::findOrFail($request->role_id);
 
-        $user = User::create([
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-            'role_id' => $request->role_id,
-        ]);
-         // Crear la persona asociada al usuario
-        $person= app(\App\Http\Controllers\PersonController::class)->store($user, $request);
-       
-        // Validacion del rol Barbero
-        if ($role->id == 2) {
-            app(\App\Http\Controllers\BarberController::class)->store($person,$request);
-        }
-        // Validacion del rol Cliente
-        if($role->id == 3 ) {
-            app(\App\Http\Controllers\ClientController::class)->store($person);
-        }
-        
-        $token = $user->createToken('access_token')->plainTextToken;
-       /* 
+            // La validación ya se maneja automáticamente con RegisterRequest
+            $request->validated();
+
+            $role = Role::findOrFail($request->role_id);
+
+            $user = User::create([
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+                'role_id' => $request->role_id,
+            ]);
+            // Crear la persona asociada al usuario
+            $person = app(\App\Http\Controllers\PersonController::class)->store($user, $request);
+
+            // Validacion del rol Barbero
+            if ($role->id == 2) {
+                app(\App\Http\Controllers\BarberController::class)->store($person, $request);
+            }
+            // Validacion del rol Cliente
+            if ($role->id == 3) {
+                app(\App\Http\Controllers\ClientController::class)->store($person);
+            }
+
+            $token = $user->createToken('access_token')->plainTextToken;
+            /* 
         $verificationUrl = URL::temporarySignedRoute(
             'verification.verify', now()->addMinutes(60), ['id' => $user->id, 'hash' => sha1($user->email)]
         );*/
 
-       // $user->sendEmailVerificationNotification();
+            // $user->sendEmailVerificationNotification();
 
-    $user->notify(new WelcomeNotification());
+            $user->notify(new WelcomeNotification());
 
-      
-        // 🔹 Enviar la notificación en segundo plano
-      //  dispatch(new SendWelcomeNotification($user));
-       
-        // Retornar una respuesta de éxito
-         return response()->json([
-        'message' => 'Usuario registrado exitosamente',
-        'access_token' => $token,
-        'token_type' => 'Bearer',
-       // 'verification_url' => $verificationUrl,
-       'user'=> new UserResource($user)
-    ], 201);
-    
-});
+
+            // 🔹 Enviar la notificación en segundo plano
+            //  dispatch(new SendWelcomeNotification($user));
+
+            // Retornar una respuesta de éxito
+            return response()->json([
+                'message' => 'Usuario registrado exitosamente',
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                // 'verification_url' => $verificationUrl,
+                'user' => new UserResource($user),
+                'errorCode' => '201'
+            ], 201);
+        });
     }
     public function login(LoginRequest $request)
     {
 
         $data = $request->only('email', 'password');
 
-    if (!Auth::attempt($data)) {
-        return response([
-            'errors' => ['Credenciales incorrectas']
-        ], 401);
+        if (!Auth::attempt($data)) {
+            return response([
+                'errors' => ['Credenciales incorrectas.'],
+                'errorCode' => '401'
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        if (!$user) {
+            return response([
+                'errors' => ['No se pudo autenticar al usuario.'],
+                'errorCode' => '422'
+            ], 422);
+        }
+
+        /** @var \Illuminate\Foundation\Auth\User $user */
+        $token = $user->createToken('access_token')->plainTextToken;
+
+
+        // Obtener datos del usuario y su relación con persona y role
+        $person = $user->person;
+        $role = $user->role->name;
+
+
+        return [
+            'access_token' => $token, // Esto retorna el token correctamente
+            'token_type' => 'Bearer',
+            'user' => new UserResource($user),
+            'errorCode' => '200'
+        ];
     }
 
-    $user = Auth::user();
+    public function logout(Request $request)
+    {
+        // Revocar el token actual del usuario
+        $request->user()->tokens()->delete();
 
-    if (!$user) {
-        return response([
-            'errors' => ['No se pudo autenticar al usuario.']
-        ], 422);
+        return response()->json([
+            'message' => 'Sesión cerrada correctamente',
+            'errorCode' => '200'
+        ], 200);
     }
-
-    /** @var \Illuminate\Foundation\Auth\User $user */
-$token = $user->createToken('access_token')->plainTextToken;
-   // Obtener datos del usuario y su relación con persona
-
-   $person = $user->person;
-   $role = $user->role->name;
-
-
-    return [
-      'access_token' => $token, // Esto retorna el token correctamente
-      'token_type' => 'Bearer',
-      'user'=> new UserResource($user)
-    ];
-
-}
-
-public function logout(Request $request)
-{
-    // Revocar el token actual del usuario
-    $request->user()->tokens()->delete();
-
-    return response()->json([
-        'message' => 'Sesión cerrada correctamente'
-    ], 200);
-}
-
 }
