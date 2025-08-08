87e570dcb918c5583632c7ab691d31e3211b1fab
Lisbeth <lisbethperezcm@gmail.com>
2025-08-07 20:56:33 -0400

    Refactorizacion del start_time y end_time de el get de citas

--- app/Http/Resources/AppointmentCollection.php
+++ app/Http/Resources/AppointmentCollection.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
@@ -15,21 +16,25 @@ class AppointmentCollection extends ResourceCollection
     public function toArray(Request $request)
     {
         return $this->collection->map(function ($appointment) {
+
+            $startDateTime = Carbon::parse($appointment->appointment_date . ' ' . $appointment->start_time)->toJSON();
+            $endDateTime   = Carbon::parse($appointment->appointment_date . ' ' . $appointment->end_time)->toJSON();
             return [
-                
+
                 'id' => $appointment->id,
-                'client_id'=>$appointment->client_id,
+                'client_id' => $appointment->client_id,
                 'client_name' => $appointment->client->person->first_name . ' ' . $appointment->client->person->last_name,
                 'barber_name' => $appointment->barber->person->first_name . ' ' . $appointment->barber->person->last_name,
                 'appointment_date' => $appointment->appointment_date, // fecha de la cita
-                'start_time' => $appointment->start_time, // hora de inicio
-                'end_time' => $appointment->end_time, // hora de fin
-                'status'=> $appointment->status->name,// Estatus de la cita
+                'start_time' => $startDateTime, // hora de inicio
+                'end_time' => $endDateTime, // hora de fin
+                'status' => $appointment->status->name, // Estatus de la cita
                 'created_at' => $appointment->created_at, // fecha y hora de creación
-                'created_by' => $appointment->createdBy ? $appointment->createdBy->person->first_name . ' ' .$appointment->createdBy->person->last_name  : 'desconocido', //
+                'created_by' => $appointment->createdBy ? $appointment->createdBy->person->first_name . ' ' . $appointment->createdBy->person->last_name  : 'desconocido', //
                 'updated_at' => $appointment->updated_at, // fecha y hora de actualización
-                'services' => $appointment->services->map(function($service) {
-                return $service->name;})
+                'services' => $appointment->services->map(function ($service) {
+                    return $service->name;
+                })
 
 
 
