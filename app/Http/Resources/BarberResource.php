22cb0c0f48268b730d9a11a5b2644c8fb246769e
Lisbeth <lisbethperezcm@gmail.com>
2025-08-24 00:10:33 -0400

    Mejoras en el BaberResource

--- app/Http/Resources/BarberResource.php
+++ app/Http/Resources/BarberResource.php
@@ -18,11 +18,12 @@ class BarberResource extends JsonResource
             'id' => $this->id,
             'first_name' => $this->person->first_name,
             'last_name' => $this->person->last_name,
-            // Si no hay persona, devuelve null
             'email' => $this->person->user->email,
             'role_id' => $this->person->user->role_id,
             'status' => $this->status,
             'commission' => $this->commission->current_percentage ?? "Desconocido",
+            'phone_number' => $this->person->phone_number,
+            'address' => $this->person->address,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
