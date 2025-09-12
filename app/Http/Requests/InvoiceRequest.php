7cc111bed1ebdc0154f633bfd5ece05cd743fa31
Lisbeth <lisbethperezcm@gmail.com>
2025-09-11 22:33:27 -0400

    Implementacion del Invoice Update

--- app/Http/Requests/InvoiceRequest.php
+++ app/Http/Requests/InvoiceRequest.php
@@ -21,13 +21,32 @@ class InvoiceRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'appointment_id'       => 'nullable|exists:appointments,id|required_without:products',
-            'client_id'            => 'required_without:appointment_id|exists:clients,id',
+        $rules = [];
 
-            'products'             => 'nullable|array|required_without:appointment_id',
-            'products.*.id'        => 'required_with:products|exists:products,id',
+        if ($this->isMethod('post')) {
+            // STORE: productos requeridos y con al menos 1 item
+            $rules = [
+                'client_id'            => 'required_without:appointment_id|exists:clients,id',
+                'appointment_id'       => 'nullable|exists:appointments,id|required_without:products',
+                'barber_id'            => 'nullable|exists:barbers,id',
+                'products'             => 'nullable|array|required_without:appointment_id',
+                'products.*.id'        => 'required_with:products|exists:products,id',
             'products.*.quantity'  => 'required_with:products|integer|min:1',
-        ];
+
+            ];
+        } else {
+            // UPDATE: productos opcional, pero si viene:
+            // - debe ser array
+            // - no puede estar vacío (min:1)
+            // - cada item debe ser válido
+            $rules = [
+                'client_id'            => 'sometimes|exists:clients,id',
+                'products' => 'sometimes|array',
+                'services' => 'sometimes|array|min:1',
+                'services.*.service_id'        => 'required_with:services|exists:services,id',
+                'services.*.quantity'  => 'required_with:services|integer|min:1',
+            ];
+        }
+        return $rules;
     }
 }
